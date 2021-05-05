90a88c185528e4bc9861de83501bd4ee3b4a2555
=DevOps Class <=fateme.safi92@gmail.com>
2021-05-06 00:15:44 +0430

    adding flask_restful package

--- authz/__init__.py
+++ authz/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
-
+from flask_restful import Api
 from authz.config.config import Config
+
+api = Api()
+
+from authz import resource
 
 def create_app():
 	
 	app = Flask(__name__)
 	
 	app.config.from_object(Config) #load configs from env variables.
+
+	api.init_app(app)
 	
 	return app
